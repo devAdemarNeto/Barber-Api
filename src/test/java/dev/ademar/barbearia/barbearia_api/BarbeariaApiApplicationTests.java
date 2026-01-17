11077ea2d01a87b29e18663994277e38b454b10d
Ademar Neto <ademarnetodev@gmail.com>
2026-01-17 11:58:54 -0300

    feat: configuração inicial do projeto

---
+++ src/test/java/dev/ademar/barbearia/barbearia_api/BarbeariaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ademar.barbearia.barbearia_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
